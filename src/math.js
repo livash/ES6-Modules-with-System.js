c47fca66ecd1607f2c7cc40e7fad6184c9bca30b
Olena Deering <olena.ivashyna@gmail.com>
2016-07-08 21:19:46 -0600

    clean up math.js

--- src/math.js
+++ src/math.js
@@ -1,13 +1,3 @@
-// export class Math {
-//   constructor() {}
-//   static sum(x, y) {
-//     return x + y;
-//   }
-//
-//   static pi = 3.141593;
-//
-// }
-
 function sum(x, y) {
   return x + y;
 }
