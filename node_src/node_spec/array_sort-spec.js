5d6a35c174fd951d2e186c5933c91ed210ef6f62
Olena Deering <olena.ivashyna@gmail.com>
2016-07-18 21:55:36 -0600

    refactor unit tests

--- node_src/node_spec/array_sort-spec.js
+++ node_src/node_spec/array_sort-spec.js
@@ -1,68 +1,49 @@
 var sort = require("../array_sort");
+var SORT_ALGS = ["mergeSort", 'bubbleSort', 'insertionSort'];
 
-describe("Merge Sort method", function() {
-  it("should return an array", function() {
-    var arr = [];
-    var result = sort.mergeSort(arr);
-    expect(result instanceof Array).toBe(true);
-  });
+SORT_ALGS.forEach(function(algName) {
+  describe(algName, function() {
+    it("should return an array", function() {
+      var arr = [2,3,1];
+      var result = sort[algName](arr);
+      expect(result instanceof Array).toBe(true);
+    });
 
-  it("should sort array of one item in length", function() {
-    var arr = [1];
-    var result = sort.mergeSort(arr);
-    expect(result.length).toBe(arr.length);
-  });
+    it("should return an empty array if given an empty array", function() {
+      var arr = [];
+      var result = sort[algName](arr);
+      expect(result.length).toBe(arr.length);
+    });
 
-  it("should sort array of two item in length", function() {
-    var arr = [2,1];
-    var result = sort.mergeSort(arr);
-    expect(result.length).toBe(arr.length);
-    expect(result.toString()).toBe([1,2].toString());
-  });
+    it("should sort array of one item in length", function() {
+      var arr = [1];
+      var result = sort[algName](arr);
+      expect(result.length).toBe(arr.length);
+    });
 
-  it("should sort an array of more than two items", function() {
-    var arr = [10,9,8,7,6,5,4,3,2,1];
-    var result = sort.mergeSort(arr);
-    expect(result.toString()).toBe(arr.reverse().toString());
-  });
-});
+    it("should sort array of two item in length", function() {
+      var arr = [2,1];
+      var result = sort[algName](arr);
+      expect(result.length).toBe(arr.length);
+      expect(result.toString()).toBe([1,2].toString());
+    });
 
-describe("Bubble Sort method", function() {
-  it("should return an array" , function() {
-    var arr = [1,2,3];
-    var result = sort.bubbleSort(arr);
-    expect(result instanceof Array).toBe(true);
-  });
+    it("should sort an array of more than two items", function() {
+      var arr = [10,9,8,7,6,5,4,3,2,1];
+      var result = sort[algName](arr);
+      expect(result.toString()).toBe(arr.reverse().toString());
+    });
 
-  it("should return empty array when given as an argument an empty array", function() {
-    var arr = [];
-    var result = sort.bubbleSort(arr);
-    expect(result.length).toBe(0);
-  });
+    it("should sort an array with negative numbers", function() {
+      var arr = [3,2,-1,0];
+      var result = sort[algName](arr);
+      expect(result.toString()).toBe([-1,0,2,3].toString());
+    });
 
-  it("should sort an array", function() {
-    var arr = [5,4,3,2,1];
-    var result = sort.bubbleSort(arr);
-    expect(result.toString()).toBe([1,2,3,4,5].toString());
-  });
-
-  it("should sort an array with negative numbers", function() {
-    var arr = [3,2,-1,0];
-    var result = sort.bubbleSort(arr);
-    expect(result.toString()).toBe([-1,0,2,3].toString());
-  });
-
-  it("should not modify the input array", function() {
-    var arr = [3,2,1];
-    var result = sort.bubbleSort(arr);
-    expect(arr.toString()).toBe([3,2,1].toString());
-  });
-});
-
-describe("Insertion Sort", function() {
-  it("should sort an array", function() {
-    var arr = [4,3,2,1];
-    var result = sort.insertionSort(arr);
-    expect(result.toString()).toBe([1,2,3,4].toString());
+    it("should not modify the input array", function() {
+      var arr = [3,2,1];
+      var result = sort[algName](arr);
+      expect(arr.toString()).toBe([3,2,1].toString());
+    });
   });
 });
