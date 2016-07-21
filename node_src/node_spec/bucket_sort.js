025b8d456a76a0da3967a84cb6de4f3d4e05f54f
Olena Deering <olena.ivashyna@gmail.com>
2016-07-20 21:40:38 -0600

    bucket sort and printBuckets methods

--- node_src/node_spec/bucket_sort.js
+++ node_src/node_spec/bucket_sort.js
@@ -16,9 +16,32 @@
   }
 */
 var sort = function(arr, min, max, buckets) {
+  var result = {};
+  var delta = Math.ceil( (max - min) / buckets);
 
+  for (var i = 0; i < arr.length; i++) {
+    var bucket = arr[i] % delta;
+    addToBucket(result, bucket, arr[i]);
+  }
+
+  return result;
+}
+
+function addToBucket(buckets, bicketId, item) {
+  if (buckets.hasOwnProperty(bucketId)) {
+    buckets[bucketId].push(item);
+  } else {
+    buckets[bucketId] = [item];
+  }
+}
+
+var printBuckets = function(buckets) {
+  for (var prop in buckets) {
+    console.log(buckets[prop].toString());
+  }
 }
 
 module.exports = {
-  sort: sort
+  sort: sort,
+  printBuckets: printBuckets
 }
