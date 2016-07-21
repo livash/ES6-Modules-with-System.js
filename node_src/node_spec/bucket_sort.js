7d66eb8dd4b7b95de8acda8e2541e9a77cf0edf5
Olena Deering <olena.ivashyna@gmail.com>
2016-07-20 21:32:13 -0600

    bucket sort

---
+++ node_src/node_spec/bucket_sort.js
@@ -0,0 +1,24 @@
+/*
+  Bucket sort
+
+  @param arr - input array of integers
+  @param min - minmal value in the array
+  @param max - value in the array
+  @param buckets - number of buckets to sort the data into
+
+  @result - object where key value pairs correspond to the bucket number and all elements in the bucket
+
+  for example:
+  result = {
+      "0": [1,2,3,4,...],
+      "1": [11, 12, 14, ..],
+      ...
+  }
+*/
+var sort = function(arr, min, max, buckets) {
+
+}
+
+module.exports = {
+  sort: sort
+}
