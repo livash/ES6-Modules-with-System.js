178d9998f4351e5d09b47025f339bea7a26bf0ad
Olena Deering <olena.ivashyna@gmail.com>
2016-07-22 12:28:37 -0600

    queue.dequeue, queue.print methods

--- node_src/queue.js
+++ node_src/queue.js
@@ -13,7 +13,7 @@ Queue.prototype.enqueue = function(val) {
   var newNode = new QNode(null, val);
   if (this.first === null) {
     this.first = newNode;
-    this.first = this.last;
+    this.last = this.first;
   } else {
     var temp = this.last;
     this.last = newNode;
@@ -23,7 +23,29 @@ Queue.prototype.enqueue = function(val) {
 
 // remove the first element in the queue
 Queue.prototype.dequeue = function() {
+  if (this.first === null) { return null; }
+  var result;
+  if (this.first === this.last) {
+    result = this.first.val;
+    this.first = null;
+    this.last = null;
+  } else {
+    var temp = this.first;
+    this.first = this.first.next;
+    result = temp.val;
+  }
   
+  return result;
+}
+
+Queue.prototype.print = function() {
+  var node = this.first;
+  var result = "";
+  while (node !== null) {
+    result += node.val + " < ";
+    node = node.next;
+  }
+  console.log(result);
 }
 
 Queue.prototype.isSorted = function() {
@@ -37,6 +59,15 @@ Queue.prototype.isSorted = function() {
 
   return true;
 }
+
+// var q = new Queue();
+// q.enqueue(1);
+// q.enqueue(2);
+// q.enqueue(3);
+// console.log(q);
+// q.print();
+// console.log(q.dequeue());
+// q.print();
 
 module.exports = {
   Queue: Queue
