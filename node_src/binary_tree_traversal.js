b855df1da99fe7cc60994cb1d7e6b37db9141dde
Olena Deering <olena.ivashyna@gmail.com>
2016-07-22 13:12:14 -0600

    Binary Tree add new node

--- node_src/binary_tree_traversal.js
+++ node_src/binary_tree_traversal.js
@@ -1,7 +1,6 @@
 var Queue = require('./queue');
 
-function BTNode(key, val) {
-  this.key = key;
+function BTNode(val) {
   this.val = val;
   this.left = null;
   this.right = null;
@@ -12,10 +11,35 @@ function BT() {
   this.root = null;
 }
 
+BT.prototype.add = function(val) {
+  if (this.root === null) {
+    this.root = new BTNode(val);
+  } else {
+    this.addNode(this.root, val);
+  }
+}
+
+BT.prototype.addNode = function(target, val) {
+  if (target.val > val) {
+    // insert on the right
+    if (target.right === null) {
+      var newNode = new BTNode(val);
+      target.right = newNode;
+    } else {
+      this.addNode(target.right, val);
+    }
+  } else {
+    if (target.left === null) {
+      var newNode = new BTNode(val);
+      target.left = newNode;
+    } else {
+      this.addNode(target.left, val);
+    }
+  }
+}
 /*
    @result - return a boolean. True if the binary tree is a binary search tree
 */
-
 BT.prototype.isBST = function() {
   // create a queue of "in-order" traversal of the tree
   var q = new Queue.Queue();
@@ -30,3 +54,9 @@ BT.prototype.isBST = function() {
 
   return q.isSorted();
 }
+
+var tree = new BT();
+tree.add(100);
+tree.add(98);
+tree.add(102);
+console.log(tree);
