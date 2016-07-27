e226ab73cf6c28b4bf2c2fea25897faaae366286
Olena Deering <olena.ivashyna@gmail.com>
2016-07-26 21:53:41 -0600

    max heap | unit tests

--- node_src/node_spec/max_heap-spec.js
+++ node_src/node_spec/max_heap-spec.js
@@ -10,4 +10,12 @@ describe("Max Heap", function() {
     var heap = new MH.MaxHeap();
     expect(heap.hasOwnProperty('array')).toBeTruthy();
   });
+
+  it("should be able to add value", function() {
+    var heap = new MH.MaxHeap();
+    heap.add(100)
+        .add(99)
+        .add(88);
+    expect(heap.array.length === 3).toBeTruthy();
+  });
 });
